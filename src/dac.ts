fa6f2c50e020ff6171d29b75e61996426b70880b
skynethub.io <skynethub.io@gmail.com>
2021-05-05 20:19:09 -0400

    comments removed

--- src/dac.ts
+++ src/dac.ts
@@ -24,10 +24,6 @@ export default class UserProfileDAC implements IUserProfileDAC {
     const methods = {
       init: this.init.bind(this),
       onUserLogin: this.onUserLogin.bind(this),
-      // getProfile: this.getProfile.bind(this),
-      // getProfileHistory: this.getProfileHistory.bind(this),
-      // getPreferences: this.getPreferences.bind(this),
-      // getPreferencesHistory: this.getPreferencesHistory.bind(this),
       setProfile: this.setProfile.bind(this),
       setPreferences: this.setPreferences.bind(this),
     };
@@ -69,117 +65,6 @@ export default class UserProfileDAC implements IUserProfileDAC {
     }
   }
 
-  /**
-   * This method is used to retrive last saved users profile information globaly. accross all skapps using this dac
-   * @param userId need to pass a dummy data for remotemethod call sample {test:"test"}
-   * @param options need to pass {ipd:"SkyId"} for skyId profiles
-   * @returns Promise<any> the last saved users profile data
-   */
-  // public async getProfile(userId: string, options: IProfileOptions): Promise<any> {
-  //   try {
-  //     // check if we need to pull "SkyID" (legancy login) profile
-  //     if (options != null && options != undefined && options.ipd == "SkyId") {
-  //       this.log('Got SkyId params checking SkyID');
-  //       // get "Skapp" name which updated profile last.
-  //       let oldData: any = await this.client.db.getJSON(userId, "profile")
-  //       let userProfile: IUserProfile = {
-  //         version: VERSION,
-  //         username: oldData.username,
-  //         aboutMe: oldData.aboutMe,
-  //         location: oldData.location || "",
-  //         topics: oldData.tags || [],
-  //         avatar: oldData.avatar || []
-  //       }
-  //       return userProfile;
-  //     }
-  //     else { // By default get "MySky" Profile
-  //       let lastSkapp = null;
-  //       if (options && options?.skapp) {
-  //         lastSkapp = options?.skapp;
-  //       }
-  //       else {
-  //         // get "Skapp" name which updated profile last. 
-  //         lastSkapp = await this.getLastestProfileSkapp();
-  //       }
-  //       // null mean profile is not initilaized correctly. 
-  //       // Ideally this shouldn't happen, since we are initializing empty profile  at first MySky login
-  //       if (lastSkapp != null) {
-  //         // download profile json from Skapp folder and return
-  //         const LATEST_PROFILE_PATH = `${DATA_DOMAIN}/${lastSkapp}/userprofile.json`;
-  //         return await this.downloadFile(LATEST_PROFILE_PATH);
-  //       }
-  //       else {// return empty profile
-  //         return this.getInitialProfile();
-  //       }
-  //     }
-  //   } catch (error) {
-  //     this.log('Error occurred trying to get profile data, err: ', error);
-  //     return { error: error }
-  //   }
-  // }
-  /**
-   * This method is used to retrive last saved users Preferences information globaly. accross all skapps using this dac
-   * @param data need to pass a dummy data for remotemethod call sample {test:"test"}
-   * @returns Promise<any> the last saved users Preferences data
-   */
-  // public async getPreferences(data: any, options: IPreferencesOptions): Promise<any> {
-  //   try {
-  //     let lastSkapp = null;
-  //     if (options && options?.skapp) {
-  //       lastSkapp = options?.skapp;
-  //     }
-  //     else {
-  //       // get "Skapp" name which updated preference last.
-  //       lastSkapp = await this.handleGetLastestPrefSkapp();
-  //     }
-  //     // null mean profile is not initilaized correctly. 
-  //     // Ideally this shouldn't happen, since we are initializing empty preference at first MySky login
-  //     if (lastSkapp != null) {
-  //       // download preferece json from Skapp folder and return
-  //       const LATEST_PREF_PATH = `${DATA_DOMAIN}/${lastSkapp}/preferences.json`;
-  //       return await this.downloadFile(LATEST_PREF_PATH);
-  //     }
-  //     else {
-  //       return this.getInitialPrefrences();
-  //     }
-  //   } catch (error) {
-  //     this.log('Error occurred trying to record new content, err: ', error)
-  //     return { error: error }
-  //   }
-  // }
-
-  /**
- * This method is used to retrive users profile information update History. accross all skapps using this dac
- * @param data need to pass a dummy data for remotemethod call sample {test:"test"}
- * @returns Promise<any> the profile data update history
- */
-  // public async getProfileHistory(data: any): Promise<any> {
-  //   try {
-  //     // purposefully not awaited
-  //     let indexData: any = await this.downloadFile(this.paths.PROFILE_INDEX_PATH);
-  //     return indexData.historyLog;
-  //   } catch (error) {
-  //     this.log('Error occurred trying to record new content, err: ', error)
-  //     return { error: error }
-  //   }
-  // }
-
-  /**
-* This method is used to retrive users Preferences information update History. accross all skapps using this dac
-* @param data need to pass a dummy data for remotemethod call sample {test:"test"}
-* @returns Promise<any> the Preferences data update history
-*/
-  // public async getPreferencesHistory(data: any): Promise<any> {
-  //   try {
-  //     // purposefully not awaited
-  //     let indexData: any = await this.downloadFile(this.paths.PREFERENCES_INDEX_PATH);
-  //     return indexData.historyLog;
-  //   } catch (error) {
-  //     this.log('Error occurred trying to record new content, err: ', error)
-  //     return { error: error }
-  //   }
-
-  // }
   // onUserLogin is called by MySky when the user has logged in successfully
   public async onUserLogin() {
     // Ensure file hierarchy will ensure the index and current page file for
@@ -192,6 +77,7 @@ export default class UserProfileDAC implements IUserProfileDAC {
       .then(() => { this.log('Successfully ensured Preferences Present') })
       .catch(err => { this.log('Failed to ensure Preferences Present, err: ', err) })
   }
+
   // ensureFileHierarchy ensures that for every entry type its current index and
   // page file exist, this ensures we do not take the hit for it when the user
   // interacts with the DAC, seeing as non existing file requests time out only
@@ -269,24 +155,6 @@ export default class UserProfileDAC implements IUserProfileDAC {
     indexRecord.historyLog.push(updateLog);
     this.updateFile(this.paths.PREFERENCES_INDEX_PATH, indexRecord)
   }
-
-  // private async getLastestProfileSkapp(): Promise<string | null> {
-  //   let indexData: any = await this.downloadFile(this.paths.PROFILE_INDEX_PATH);
-  //   if (indexData != null) {
-  //     return indexData.lastUpdatedBy;
-  //   } else {
-  //     return null;
-  //   }
-  // }
-
-  // private async handleGetLastestPrefSkapp(): Promise<string | null> {
-  //   let indexData: any = await this.downloadFile(this.paths.PREFERENCES_INDEX_PATH);
-  //   if (indexData != null) {
-  //     return indexData.lastUpdatedBy;
-  //   } else {
-  //     return null
-  //   }
-  // }
 
   // downloadFile merely wraps getJSON but is typed in a way that avoids
   // repeating the awkward "as unknown as T" everywhere
